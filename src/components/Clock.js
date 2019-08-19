e75b208e40c62a865d7bd385d5be83549eee5371
gijsmaas82 <gijsmaas82@outlook.com>
2019-08-19 17:52:11 +0200

    clock is ticking

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,17 +1,49 @@
 import React, { Component } from 'react'
-import ClockSeconds from './ClockSeconds'
-import ClockMinutes from './ClockMinutes'
-import ClockHours from './ClockHours'
+
 
 export default class Clock extends Component {
+  state = {
+    time: new Date()
+  }
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+  
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
 
+  tick() {
+    setInterval(this.setState({
+      time: new Date()
+    }), 1000)
+  }
   render() {
-
+    const hoursStyle = {
+      color:'green'
+    }
+    let h = this.state.time.getHours()
+    
+    const minutesStyle = {
+      color:'blue'
+    }
+    let m = this.state.time.getMinutes()
+    m = (m < 10 ? "0" : "") + m
+    const secondsStyle = {
+      color:'orange'
+    }
+    let s = this.state.time.getSeconds()
+    s = (s < 10 ? "0" : "" ) + s
     const clockStyle = {
-      display: 'flex'
+      display: 'flex',
+      justifyContent: 'center'
     }
     return <div>
-      <h1 style={clockStyle}>It is: <ClockHours /><ClockMinutes /><ClockSeconds /></h1>
+      <h1 style={clockStyle}>
+        <div style={hoursStyle}> {h}:</div>
+        <div style={minutesStyle}>{m}:</div>
+        <div style={secondsStyle}>{s}</div>
+      </h1>
     </div>
   }
 
